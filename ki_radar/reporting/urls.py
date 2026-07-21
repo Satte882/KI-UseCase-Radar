c7692ae24e1fa5a549e7a39816dc7dbfe2ad2966
Satte882 <satinder.sandhoo@gmx.de>
2026-07-21 17:03:38 +0200

    Route portfolio reporting view

--- ki_radar/reporting/urls.py
+++ ki_radar/reporting/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import dashboard
+from .views import dashboard, portfolio
 
 app_name = "reporting"
 urlpatterns = [
     path("", dashboard, name="dashboard"),
+    path("portfolio/", portfolio, name="portfolio"),
 ]
