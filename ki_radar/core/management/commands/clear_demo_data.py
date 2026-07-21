9e15d81c411adffa152cba1923d7ce5790938601
Satte882 <satinder.sandhoo@gmx.de>
2026-07-21 22:26:57 +0200

    Use stable identities for demo cleanup

--- ki_radar/core/management/commands/clear_demo_data.py
+++ ki_radar/core/management/commands/clear_demo_data.py
@@ -2,12 +2,14 @@ from django.core.management.base import BaseCommand
 
 from ki_radar.core.demo_architecture_data import clear_demo_architecture_data
 from ki_radar.core.demo_data import clear_demo_data
+from ki_radar.core.demo_identity import prepare_demo_identities
 
 
 class Command(BaseCommand):
     help = "Remove only the KI-Radar demo dataset created by seed_demo_data."
 
     def handle(self, *args, **options):
+        prepare_demo_identities()
         architecture_counts = clear_demo_architecture_data()
         counts = clear_demo_data()
         self.stdout.write(
