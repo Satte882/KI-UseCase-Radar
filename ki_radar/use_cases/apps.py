e73b532e7b4e1c90937a77a658891bce981714f4
Satte882 <satinder.sandhoo@gmx.de>
2026-07-22 08:45:34 +0200

    Register use case classification model

--- ki_radar/use_cases/apps.py
+++ ki_radar/use_cases/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UseCasesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "ki_radar.use_cases"
+
+    def ready(self):
+        from . import classification  # noqa: F401, PLC0415
