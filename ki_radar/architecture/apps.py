34dba51c17d4d0639efdc86df800ee367d883d88
Satte882 <satinder.sandhoo@gmx.de>
2026-07-22 08:43:53 +0200

    Register value stream focus model

--- ki_radar/architecture/apps.py
+++ ki_radar/architecture/apps.py
@@ -5,3 +5,6 @@ class ArchitectureConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "ki_radar.architecture"
     verbose_name = "Discovery und Architektur"
+
+    def ready(self):
+        from . import focus  # noqa: F401, PLC0415
