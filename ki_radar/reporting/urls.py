20d87e3a5151fd731fa4b466cc84d15ed708fb97
Satte882 <satinder.sandhoo@gmx.de>
2026-07-22 11:38:45 +0200

    Route outcome workspace UX spike

--- ki_radar/reporting/urls.py
+++ ki_radar/reporting/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import dashboard, portfolio
+from .views import dashboard, outcome_workspace, portfolio
 
 app_name = "reporting"
 urlpatterns = [
     path("", dashboard, name="dashboard"),
     path("portfolio/", portfolio, name="portfolio"),
+    path("wirkung-betrieb/", outcome_workspace, name="outcome_workspace"),
 ]
