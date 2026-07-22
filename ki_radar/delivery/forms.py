6a55f2f4382005c94ee06b662aaf014071064c38
Satte882 <satinder.sandhoo@gmx.de>
2026-07-22 08:56:48 +0200

    Edit delivery architecture artifacts in package form

--- ki_radar/delivery/forms.py
+++ ki_radar/delivery/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+from .architecture_artifacts import get_delivery_architecture_artifacts
 from .models import DeliveryPackage
 
 FORM_CONTROL = "form-control"
@@ -11,8 +12,12 @@ DELIVERY_PACKAGE_FIELDS = [
     "users_and_scenarios",
     "solution_outline",
     "system_context",
+    "system_landscape",
     "data_context",
+    "data_flows",
     "integrations",
+    "integration_contracts",
+    "architecture_artifacts_url",
     "functional_requirements",
     "non_functional_requirements",
     "security_privacy_requirements",
@@ -34,19 +39,69 @@ DELIVERY_PACKAGE_FIELDS = [
 
 
 class DeliveryPackageForm(forms.ModelForm):
+    system_landscape = forms.CharField(
+        widget=forms.Textarea(attrs={"rows": 6}),
+        label="Ist-/Ziel-Systemlandschaft",
+    )
+    data_flows = forms.CharField(
+        widget=forms.Textarea(attrs={"rows": 6}),
+        label="Daten- und Informationsflüsse",
+    )
+    integration_contracts = forms.CharField(
+        widget=forms.Textarea(attrs={"rows": 5}),
+        label="Integrationsverträge und Verantwortlichkeiten",
+    )
+    architecture_artifacts_url = forms.URLField(
+        required=False,
+        label="Architekturartefakte und Diagramme",
+    )
+
     class Meta:
         model = DeliveryPackage
         fields = DELIVERY_PACKAGE_FIELDS
         widgets = {
             name: forms.Textarea(attrs={"rows": 4})
             for name in DELIVERY_PACKAGE_FIELDS
-            if name != "external_delivery_url"
+            if name not in {
+                "external_delivery_url",
+                "architecture_artifacts_url",
+                "system_landscape",
+                "data_flows",
+                "integration_contracts",
+            }
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        artifacts = (
+            get_delivery_architecture_artifacts(self.instance)
+            if self.instance.pk
+            else None
+        )
+        if artifacts is not None and not self.is_bound:
+            self.initial.update(
+                {
+                    "system_landscape": artifacts.system_landscape,
+                    "data_flows": artifacts.data_flows,
+                    "integration_contracts": artifacts.integration_contracts,
+                    "architecture_artifacts_url": artifacts.artifacts_url,
+                }
+            )
         for field in self.fields.values():
             field.widget.attrs.setdefault("class", FORM_CONTROL)
         self.fields["functional_requirements"].widget.attrs["rows"] = 7
         self.fields["acceptance_criteria"].widget.attrs["rows"] = 7
         self.fields["initial_backlog"].widget.attrs["rows"] = 7
+
+    def save(self, commit=True):
+        package = super().save(commit=False)
+        package._architecture_artifacts_payload = {
+            "system_landscape": self.cleaned_data["system_landscape"],
+            "data_flows": self.cleaned_data["data_flows"],
+            "integration_contracts": self.cleaned_data["integration_contracts"],
+            "artifacts_url": self.cleaned_data.get("architecture_artifacts_url", ""),
+        }
+        if commit:
+            package.save()
+            self.save_m2m()
+        return package
