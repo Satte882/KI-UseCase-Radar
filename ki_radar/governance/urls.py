f6518448b27e3f44634d247c7576d015898ff682
Satte882 <satinder.sandhoo@gmx.de>
2026-08-03 12:27:24 +0200

    Route dedicated governance review workspaces

--- ki_radar/governance/urls.py
+++ ki_radar/governance/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
-from .views import assessment_create
+from .views import assessment_create, review_create
 
 app_name = "governance"
-urlpatterns = [path("use-case/<uuid:use_case_id>/new/", assessment_create, name="create")]
+urlpatterns = [
+    path("use-case/<uuid:use_case_id>/new/", assessment_create, name="create"),
+    path(
+        "use-case/<uuid:use_case_id>/review/<str:review_type>/",
+        review_create,
+        name="review",
+    ),
+]
