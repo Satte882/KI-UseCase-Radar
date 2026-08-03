afae7e7be56b7e16a76ea16f68d3e37759e9868e
Satte882 <satinder.sandhoo@gmx.de>
2026-08-03 14:37:41 +0200

    Remove extra lifecycle wrapper installation

--- ki_radar/use_cases/apps.py
+++ ki_radar/use_cases/apps.py
@@ -8,11 +8,9 @@ class UseCasesConfig(AppConfig):
     def ready(self):
         from . import classification  # noqa: F401
         from .governance_journey import install as install_governance_journey
-        from .lifecycle_journey import install as install_lifecycle_journey
         from .value_stream_journey import install as install_value_stream_journey
         from .workflow import install
 
         install()
         install_value_stream_journey()
         install_governance_journey()
-        install_lifecycle_journey()
