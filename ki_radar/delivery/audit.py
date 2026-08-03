9d7d845c9b3120dd49c783be16469eef0b5f767c
Satte882 <satinder.sandhoo@gmx.de>
2026-08-03 21:41:41 +0200

    Add immutable audit manager

---
+++ ki_radar/delivery/audit.py
@@ -0,0 +1,17 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+
+
+class ImmutableAuditQuerySet(models.QuerySet):
+    def update(self, **kwargs):
+        raise ValidationError("Dokumentierte Quellenentscheidungen sind unveränderlich.")
+
+    def bulk_update(self, objs, fields, batch_size=None):
+        raise ValidationError("Dokumentierte Quellenentscheidungen sind unveränderlich.")
+
+    def delete(self):
+        raise ValidationError("Dokumentierte Quellenentscheidungen sind unveränderlich.")
+
+
+class ImmutableAuditManager(models.Manager.from_queryset(ImmutableAuditQuerySet)):
+    pass
