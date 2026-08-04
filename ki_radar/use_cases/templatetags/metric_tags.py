0c834546ffd535655339ff08b250a86d6c722041
Satte882 <satinder.sandhoo@gmx.de>
2026-08-04 13:30:35 +0200

    Add single metric presentation tag

--- ki_radar/use_cases/templatetags/metric_tags.py
+++ ki_radar/use_cases/templatetags/metric_tags.py
@@ -1,6 +1,9 @@
 from django import template
 
-from ki_radar.use_cases.metric_presentation import build_metric_set_presentation
+from ki_radar.use_cases.metric_presentation import (
+    build_metric_presentation,
+    build_metric_set_presentation,
+)
 
 register = template.Library()
 
@@ -8,3 +11,8 @@ register = template.Library()
 @register.simple_tag
 def metric_set(source):
     return build_metric_set_presentation(source)
+
+
+@register.simple_tag
+def metric_value(value, metric_type=None, unit=""):
+    return build_metric_presentation(metric_type=metric_type, value=value, unit=unit)
