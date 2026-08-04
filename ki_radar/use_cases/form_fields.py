4d0c1f24d0b2f6450ab095993b9287dfe6382b08
Satte882 <satinder.sandhoo@gmx.de>
2026-08-04 13:28:40 +0200

    Add localized decimal form fields

---
+++ ki_radar/use_cases/form_fields.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from decimal import Decimal, InvalidOperation
+
+from django import forms
+from django.utils.formats import get_format
+
+
+def format_decimal_input(value: Decimal) -> str:
+    """Format a Decimal for localized form input without grouping or rounding."""
+
+    normalized = format(value, "f").rstrip("0").rstrip(".")
+    if normalized in {"", "-0"}:
+        normalized = "0"
+    return normalized.replace(".", str(get_format("DECIMAL_SEPARATOR")))
+
+
+class LocalizedDecimalInput(forms.TextInput):
+    def __init__(self, attrs=None):
+        default_attrs = {"inputmode": "decimal", "autocomplete": "off"}
+        if attrs:
+            default_attrs.update(attrs)
+        super().__init__(attrs=default_attrs)
+
+    def format_value(self, value):
+        if value in self.empty_values:
+            return ""
+        if isinstance(value, Decimal):
+            return format_decimal_input(value)
+        try:
+            decimal_value = Decimal(str(value))
+        except (InvalidOperation, TypeError, ValueError):
+            return super().format_value(value)
+        return format_decimal_input(decimal_value)
+
+
+class LocalizedDecimalField(forms.DecimalField):
+    widget = LocalizedDecimalInput
+
+    def __init__(self, *args, **kwargs):
+        kwargs["localize"] = True
+        super().__init__(*args, **kwargs)
