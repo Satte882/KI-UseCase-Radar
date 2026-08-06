5ec984f71eef76feebf6cc1de2beceab78d4aa64
Satte882 <satinder.sandhoo@gmx.de>
2026-08-06 10:36:24 +0200

    Issue #121 AP 8: Review-UI und Unsicherheitsregeln

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -3,3 +3,4 @@ from .base import *  # noqa: F403
 DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "testserver"]
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+ACCELERATOR_FIELD_ADOPTION_ENABLED = True
