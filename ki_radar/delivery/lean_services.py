256f799ec6a9dc19223ea47b732f4642ccc0f804
Satte882 <satinder.sandhoo@gmx.de>
2026-09-11 16:01:34 +0200

    Fix Ruff import ordering in lean delivery services

--- ki_radar/delivery/lean_services.py
+++ ki_radar/delivery/lean_services.py
@@ -4,8 +4,8 @@ from django.core.exceptions import ValidationError
 from django.db import transaction
 from django.utils import timezone
 
-from .models import DeliveryPackage
-from .readiness import evaluate_delivery_readiness
+from ki_radar.delivery.models import DeliveryPackage
+from ki_radar.delivery.readiness import evaluate_delivery_readiness
 
 
 ENFORCEMENT_FINDING_CODES = {"SECTION_BLOCKED"}
