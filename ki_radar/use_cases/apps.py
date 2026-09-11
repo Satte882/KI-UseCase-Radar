07042000406c7861ee90bb0c2c5eda54e63bab26
Satte882 <satinder.sandhoo@gmx.de>
2026-09-11 15:44:32 +0200

    feat: install lean journey projection after existing wrappers

--- ki_radar/use_cases/apps.py
+++ ki_radar/use_cases/apps.py
@@ -8,6 +8,7 @@ class UseCasesConfig(AppConfig):
     def ready(self):
         from . import classification  # noqa: F401
         from .governance_journey import install as install_governance_journey
+        from .lean_journey import install as install_lean_journey
         from .primary_actions import install as install_primary_actions
         from .value_stream_journey import install as install_value_stream_journey
         from .workflow import install
@@ -16,3 +17,4 @@ class UseCasesConfig(AppConfig):
         install_value_stream_journey()
         install_governance_journey()
         install_primary_actions()
+        install_lean_journey()
