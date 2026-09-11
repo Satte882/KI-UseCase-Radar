ba287b4bb2d423fe8b8f887d638d7d5b73047a5e
Satte882 <satinder.sandhoo@gmx.de>
2026-09-11 16:06:01 +0200

    Match Ruff import sectioning in lean decision forms

--- ki_radar/use_cases/lean_decision_forms.py
+++ ki_radar/use_cases/lean_decision_forms.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from django import forms
 from django.core.exceptions import ValidationError
 from django.forms.models import construct_instance
-
 from ki_radar.use_cases.decision_forms import (
     ApprovalDecisionForm as LegacyApprovalDecisionForm,
     DecisionAssessmentForm as LegacyDecisionAssessmentForm,
